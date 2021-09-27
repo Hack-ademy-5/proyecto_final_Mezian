9e798081492b716464e10a384a417e4256b9c678
mezian <mezianbarka@gmail.com>
2021-09-27 09:09:18 +0200

    arreglar imagen no encontrada al subirlo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -110,7 +110,7 @@ public function uploadImages(Request $request)
     
   $uniqueSecret = $request->input('uniqueSecret');
   $filePath = $request->file('file')->store("public/temp/{$uniqueSecret}");
-  dispatch(new ResizeImage($filePath,120,120));
+  
   session()->push("images.{$uniqueSecret}", $filePath);
    
    return response()->json(
